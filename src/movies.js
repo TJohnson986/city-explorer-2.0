84e0159b803648bed437dbe67d066f72612a716d
Tyler Johnson <tyler@csdconsultants.us>
2021-10-14 07:26:23 -0700

    creates movies class to render single movie lists

---
+++ src/movies.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SingleMovie from './SingleMovie';
+import { ListGroup } from 'react-bootstrap';
+
+class Movies extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render(){
+        const allListGroups = this.props.movieData.map((info, index) => (
+            <SingleMovie key={index} title={info.title} overview={info.overview} />
+        ))
+
+        return (
+            <ListGroup>
+                {allListGroups}
+            </ListGroup>
+        );
+    }
+}
+
+export default Movies;
